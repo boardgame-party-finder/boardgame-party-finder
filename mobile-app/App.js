62acad133d266f52dc6875a977d1374e7384eba7
Punn Nararatwong <punn.nararatwong@thomsonreuters.com>
2018-08-10 17:24:00 +0700

    Add native-base and setup with Expo

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import Expo from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
+import AnatomyExample from './src/main';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { loading: true };
+    }
+
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        });
+        this.setState({ loading: false });
+    }
+    
     render() {
+        if (this.state.loading) {
+            return <Expo.AppLoading />;
+        }
         return (
-            <View style={styles.container}>
-                <Text>Hello world</Text>
-            </View>
+            <AnatomyExample>
+            </AnatomyExample>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
