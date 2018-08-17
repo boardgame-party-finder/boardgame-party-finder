b7cf0ec107571f791407aad6c8f91a9de20bf3d3
Punn Nararatwong <punn.nararatwong@thomsonreuters.com>
2018-08-17 16:42:07 +0700

    Add boilerplate

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import Expo from 'expo';
-import { StyleSheet, Text, View } from 'react-native';
-import AnatomyExample from './src/main';
+import Root from './src/native/index';
+import configureStore from './src/store/index';
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { loading: true };
-    }
+const { persistor, store } = configureStore();
 
-    async componentWillMount() {
-        await Expo.Font.loadAsync({
-            'Roboto': require('native-base/Fonts/Roboto.ttf'),
-            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-        });
-        this.setState({ loading: false });
-    }
-    
-    render() {
-        if (this.state.loading) {
-            return <Expo.AppLoading />;
-        }
-        return (
-            <AnatomyExample>
-            </AnatomyExample>
-        );
-    }
+export default function App() {
+  return <Root store={store} persistor={persistor} />;
 }
